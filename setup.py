8be419efc5c045e74e492cdd64cdbd8d5c30f687
Vadim <v.marcenko@gmx.de>
2020-01-20 13:51:18 +0100

    setup.py added

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="atlas-core-lib",
+    version="0.0.1",
+    author="Vadim Marcenko",
+    author_email="v.marcenko@fz-juelich.de",
+    description="Core functionality for the atlas viewer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HumanBrainProject/atlas-core-lib.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
