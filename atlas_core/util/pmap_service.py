4daa0cf1b4247ce039cc954c71f455f3888a50d2
vadim <v.marcenko@fz-juelich.de>
2020-01-23 10:00:05 +0100

    returning the decompressed nifti instead of a gzip file

--- atlas_core/util/pmap_service.py
+++ atlas_core/util/pmap_service.py
@@ -2,32 +2,39 @@ import gzip
 
 import requests
 
+from atlas_core.Roi import Roi
+
 # URL for the pmap-service
+from atlas_core.region import Region
+
 url = 'http://pmap-pmap-service.apps-dev.hbp.eu/multimerge'
 
 
-def retrieve_probability_map(area_name, hemisphere, threshold):
+def retrieve_probability_map(region, hemisphere, threshold):
     """
     Retrieves a probability map for an area
-    :param area_name: Name of the area
+    :param region: Region object
     :param hemisphere: The Hemisphere. Should be provided through the enum
     :param threshold: Threshold or the probability map
-    :return: returns the nii file if status is OK, otherwise None
+    :return: returns a Roi object containing the nii file if status is OK, otherwise None
     """
     try:
-        data = '{ "areas": [{"name": "' + area_name + \
+        data = '{ "areas": [{"name": "' + region.name + \
                '","hemisphere": "' + hemisphere + '" }], "threshold": ' + str(threshold) + '}'
         response = requests.post(url, data=data, headers={'Content-Type': 'application/json'})
     except requests.exceptions.RequestException as e:
         print(e)
         response = None
     if response is not None and response.status_code == 200:
-        return gzip.decompress(response.content)
+        data_nii = gzip.decompress(response.content)
+        return Roi(data_nii, region, hemisphere, threshold)
     else:
         print('response', response)
         return None
 
 
 if __name__ == '__main__':
-    roi = retrieve_probability_map('Area-Fp1', 'left', 0.2)
+    roi = retrieve_probability_map(Region('Area-Fp1', 'colin', 'par1'), 'left', 0.2)
+    roi.save("zzz")
+    roi.save("yyy.nii")
     print(roi)
